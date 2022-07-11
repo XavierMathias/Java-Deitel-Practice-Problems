24d3e5c8a454025f52e82b81979ab5b4307c530e
David Santos <dsdsantos973@gmail.com>
2022-07-11 15:47:31 -0400

    finished setting up the class along with it's getter and setter, needs to test the newBalanceCalc

--- src/Chapter4/CreditLimitCalc.java
+++ src/Chapter4/CreditLimitCalc.java
@@ -1,8 +1,14 @@
 package Chapter4;
 
+import java.util.Scanner;
+
 public class CreditLimitCalc {
 
+    Scanner input = new Scanner(System.in);
+
     public CreditLimitCalc (){
+
+        System.out.println("Enter the following information:");
 
     } // end of constructor
 
@@ -23,9 +29,58 @@ public class CreditLimitCalc {
             this.totalItemsCharged = totalItemsCharged;
             this.totalCreditsApplied = creditsApplied;
             this.creditLimit = creditLimit;
+
+            System.out.println("This is your new balance: $" + newBalanceCalc());
+            if(newBalanceCalc() > creditLimit ){
+                System.out.println("Credit limit exceeded");
+            }
+        } // end of constructor
+
+        // calculates the new balance and determining if the balance exceeded the credit limit
+        private int newBalanceCalc(){
+            int newBalance = balance + totalItemsCharged - totalCreditsApplied;
+            return newBalance;
         }
 
+        public int getAccountNum() {
+            return accountNum;
+        }
 
+        public void setAccountNum(int accountNum) {
+            this.accountNum = accountNum;
+        }
+
+        public int getBalance() {
+            return balance;
+        }
+
+        public void setBalance(int balance) {
+            this.balance = balance;
+        }
+
+        public int getTotalItemsCharged() {
+            return totalItemsCharged;
+        }
+
+        public void setTotalItemsCharged(int totalItemsCharged) {
+            this.totalItemsCharged = totalItemsCharged;
+        }
+
+        public int getTotalCreditsApplied() {
+            return totalCreditsApplied;
+        }
+
+        public void setTotalCreditsApplied(int totalCreditsApplied) {
+            this.totalCreditsApplied = totalCreditsApplied;
+        }
+
+        public int getCreditLimit() {
+            return creditLimit;
+        }
+
+        public void setCreditLimit(int creditLimit) {
+            this.creditLimit = creditLimit;
+        }
     } // end of Customer class
 
 } // end of class
