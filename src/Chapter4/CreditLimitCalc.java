de53f37cecf662ed5fb4db7e63ce7974392cafe0
David Santos <dsdsantos973@gmail.com>
2022-07-11 15:39:10 -0400

    created a nested class of Customer within CreditLimitCalc class

--- src/Chapter4/CreditLimitCalc.java
+++ src/Chapter4/CreditLimitCalc.java
@@ -1,4 +1,31 @@
 package Chapter4;
 
 public class CreditLimitCalc {
-}
+
+    public CreditLimitCalc (){
+
+    } // end of constructor
+
+
+
+
+    class Customer{
+
+        private int accountNum;
+        private int balance;
+        private int totalItemsCharged;
+        private int totalCreditsApplied;
+        private int creditLimit;
+
+        Customer(int accountNum, int beginningBalance, int totalItemsCharged, int creditsApplied, int creditLimit){
+            this.accountNum = accountNum;
+            this.balance = beginningBalance;
+            this.totalItemsCharged = totalItemsCharged;
+            this.totalCreditsApplied = creditsApplied;
+            this.creditLimit = creditLimit;
+        }
+
+
+    } // end of Customer class
+
+} // end of class
