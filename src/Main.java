4be6605de9124a77b05f0ae6340b1b802ffd3a6d
David Santos <dsdsantos973@gmail.com>
2022-07-21 23:15:36 -0400

    added salescommisionCalc calss into main class

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import Chapter4.CreditLimitCalc;
 import Chapter4.GasMileage;
+import Chapter4.SalesCommissionCalc;
 
 public class Main {
 
     public static void main(String[] args) {
 
-
+        SalesCommissionCalc salesCommissionCalc = new SalesCommissionCalc();
         CreditLimitCalc clc = new CreditLimitCalc();
        // GasMileage gm = new GasMileage();
     } // end of main
