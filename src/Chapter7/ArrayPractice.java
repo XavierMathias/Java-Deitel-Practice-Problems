d242a5088c32ccb6c20dcce6d0b48d85f309a810
David Santos <dsdsantos973@gmail.com>
2022-07-27 21:03:01 -0400

    created three examples working with arrays

--- src/Chapter7/ArrayPractice.java
+++ src/Chapter7/ArrayPractice.java
@@ -3,11 +3,26 @@ package Chapter7;
 public class ArrayPractice {
 
     String[] names = {"David", "Melina", "Esther", "Jon"};
+    String[] foodList = new String[4];
+    String[] foods = {"pizza", "burger", "fries", "pancakes"};
+    int[] array = {32, 27, 64, 18, 95, 14, 90, 70, 70, 60, 37};
 
     public ArrayPractice (){
 
-        for(int index = 0; index <= names.length; index++){
+        for(int index = 0; index < names.length; index++){
             System.out.println(names[index]);
+        }
+
+        for (int index = 0; index < foodList.length; index++){
+            foodList[index] = foods[index];
+            System.out.println(foodList[index]);
+        }
+
+        int counter = 0;
+        System.out.printf("%s%8s%n", "Index", "Value");
+        for (int a: array){
+            System.out.printf("%5d%5d%n", counter, a);
+            counter++;
         }
 
     } // end of constructor
