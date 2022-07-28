6bc8c0488548b0a45edf64d27b7d3503c55956d2
David Santos <dsdsantos973@gmail.com>
2022-07-27 20:39:15 -0400

    created a array practice package

---
+++ src/Chapter7/ArrayPractice.java
@@ -0,0 +1,15 @@
+package Chapter7;
+
+public class ArrayPractice {
+
+    String[] names = {"David", "Melina", "Esther", "Jon"};
+
+    public ArrayPractice (){
+
+        for(int index = 0; index <= names.length; index++){
+            System.out.println(names[index]);
+        }
+
+    } // end of constructor
+
+} // end of class
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 import Chapter4.CreditLimitCalc;
 import Chapter4.GasMileage;
 import Chapter4.SalesCommissionCalc;
+import Chapter7.ArrayPractice;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        SalesCommissionCalc salesCommissionCalc = new SalesCommissionCalc();
+        ArrayPractice arrayPractice = new ArrayPractice();
+        // SalesCommissionCalc salesCommissionCalc = new SalesCommissionCalc();
         // CreditLimitCalc clc = new CreditLimitCalc();
-       // GasMileage gm = new GasMileage();
+        // GasMileage gm = new GasMileage();
     } // end of main
 
 } // end of class
